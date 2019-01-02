5410a97f94d87b600beafbaefa620b9c474cf96e
Fadi Qassem <fqassem@ncsu.edu>
2019-01-02 02:01:15 -0800

    Inital commit of naked camera app

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, StatusBar, View } from 'react-native';
+import { Camera, Permissions } from 'expo';
+
+export default class App extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <StatusBar hidden={true} />
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+};
